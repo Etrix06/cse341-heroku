47791edd9e1a989e31b8549dbfbb3f16200300f5
etrix06 <alc18005@byui.edu>
2021-09-26 17:40:33 -0700

    copied files from node

---
+++ app.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');  //called on terminal using npm install --save body-parser
+
+const app = express();  //initializes a new object in the constant app
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));//added last
+
+
+app.use('/admin', adminData.routes); //this imports the routes export in admin.js
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res.status(404).render('404', {pageTitle: 'Page Not Found', path: ''});
+});
+
+app.listen(3000);
+
+console.log('listening on 3000');
