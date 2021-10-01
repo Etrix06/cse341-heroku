48710e80d1300655d93d1e3556a776111cbe9501
etrix06 <alc18005@byui.edu>
2021-10-01 10:15:15 -0700

    ch

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const path = require('path');
+//const fs = require('fs'); 
 
 const express = require('express');
 const bodyParser = require('body-parser');  //called on terminal using npm install --save body-parser
@@ -8,6 +9,13 @@ const app = express();  //initializes a new object in the constant app
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
+
+
+//fs.readFile("./https://byui-cse.github.io/cse341-course/lesson03/items.json", "utf8", err, JSON);
+//if (err) {
+//  console.log('failed');
+//  return; }
+//}; 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
