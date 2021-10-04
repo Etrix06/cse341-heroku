ee84b99000c1c3765be57625172b81b7b28b08d5
Erik <alc18005@byui.edu>
2021-10-03 19:16:24 -0700

    updated with starting code for module 12

--- routes/shop.js
+++ routes/shop.js
@@ -10,7 +10,7 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-router.get('/products/:productId', shopController.getProduct);  // the colon signals to express that it is not a route
+router.get('/products/:productId', shopController.getProduct);
 
 router.get('/cart', shopController.getCart);
 
@@ -18,8 +18,8 @@ router.post('/cart', shopController.postCart);
 
 router.post('/cart-delete-item', shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders);
+router.post('/create-order', shopController.postOrder);
 
-router.get('/checkout', shopController.getCheckout);
+router.get('/orders', shopController.getOrders);
 
 module.exports = router;
