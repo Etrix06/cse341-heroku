078f63b189e79f226d96a2d50a98c22cc8f6b0ab
etrix06 <alc18005@byui.edu>
2021-10-08 17:30:31 -0700

    testing changing port in app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.use(shopRoutes);
 app.use(errorController.get404);
 
 mongoConnect(() => {
-  app.listen(process.env.PORT || 3000);
+  app.listen(/*process.env.PORT || */3000);
   console.log('listening on 3000');
 });
 
